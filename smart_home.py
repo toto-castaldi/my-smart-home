e6c444b74f62f2e7ef2fb2697f3a81e6400e2e94
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-05-29 19:09:50 +0200

    fix up lifx

--- smart_home.py
+++ smart_home.py
@@ -10,39 +10,36 @@ c = config.get_config()
 ligths_lan = lifxlan.LifxLAN(None)
 client = fliclib.FlicClient("localhost")
 MAX_POWER = 65535
-devices = {
+devices_cache = {
 
 }
 
 def toggle_lifx(device):
 	current_power = device.get_power()
 	device.set_power(0 if current_power == MAX_POWER else MAX_POWER, rapid=True)
-	
 
 def down_lifx(device):
 	current_brightness = device.get_color()[2]
 	device.set_brightness(current_brightness - 12000 if current_brightness >= 12000 else 0, rapid=True)
 
-def up_lifx(action_id):
+def up_lifx(device):
 	current_brightness = device.get_color()[2]
 	logger.debug(current_brightness)
 	device.set_brightness(current_brightness + 12000 if current_brightness < (MAX_POWER - 12000) else MAX_POWER, rapid=True)
-	
 
 def off_lifx(device):
 	device.set_power(0, rapid=True)
-	
 
 def wrap_lifx(callback, targets):
 	try:
 		for target in targets.split(","):
 			device = None
-			if target in devices:
-				device = devices[target]
+			if target in devices_cache:
+				device = devices_cache[target]
 			else:
 				logger.info(f"search for device {target}")
 				device = ligths_lan.get_device_by_name(target)
-				devices[target] = device
+				devices_cache[target] = device
 
 			if device:
 				callback(device)
@@ -61,12 +58,12 @@ actions = {
 def on_button_single_or_double_click_or_hold(channel, click_type, was_queued, time_diff):
 	bd_addr = channel.bd_addr
 	c_type = str(click_type)
-	logger.info(f"{bd_addr} -> {c_type}")
+	logger.debug(f"{bd_addr} -> {c_type}")
 	button = config.search_button(bd_addr)
 	if button is not None:
 		configured_action = config.search_action(button, c_type)
 		if configured_action is not None:
-			logger.debug(configured_action)
+			logger.info(configured_action)
 			wrap_lifx(actions[configured_action["action"]], configured_action["action_id"])
 		else:
 			logger.warning(f"unknow {c_type} action on button {bd_addr}")	
